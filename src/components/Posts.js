d1e9c6a937c7a8d2c696755671aa2023d4abd559
Sujal Lama <sujallama@Sujals-MacBook-Air.local>
2023-07-16 21:57:34 +0545

    Post data fetched from api and post style

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
+import Post from "./Post";
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Posts () {
     const [posts, setPosts] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(BASE_API_URL + "/api/feed");
+            if(response?.ok) {
+                const results = await response.json();
+                setPosts(results.data);
+            } else {
+                setPosts(null)
+            }
+        })();
+    }, [])
 
     return <>
         {
             posts === undefined ? 
                 <Spinner animation="border" />
             :
-            <>{posts.length === 0 
-              ? <p>There are no blog posts.</p> 
-              : posts.map(post => {
-                  return (<p key={post.id}>
-                  <b>{post.author.username}</b> &mdash;
-                  {post.timestamp}
-                  <br />
-                  {post.text}
-                </p>)
+            <>{
+                posts === null ? <p>Could not retrieve blog posts</p> 
+                :
+                posts.length === 0 
+                    ? <p>There are no blog posts.</p> 
+                    : posts.map(post => {
+                        return (<Post post={post} key={post.id} />)
             })}</>
             
         }
